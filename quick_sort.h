7d3dfe27aff3508b0e57e76ccd72715aa8ef34be
congcong-hehe <heyaqihehe@163.com>
2022-09-03 23:45:55 +0800

    fix: quick_sort rand

--- quick_sort.h
+++ quick_sort.h
@@ -9,9 +9,9 @@ int partition(vector<int> &nums, int left, int right)
 	int pivot = nums[left];
 	while (left < right)
 	{
-		while (left < right && nums[right] > pivot) right--;
+		while (left < right && nums[right] >= pivot) right--;
 		nums[left] = nums[right];
-		while (left < right && nums[left] < pivot) left++;
+		while (left < right && nums[left] <= pivot) left++;
 		nums[right] = nums[left];
 	}
 	nums[left] = pivot;
@@ -21,6 +21,9 @@ int partition(vector<int> &nums, int left, int right)
 void quickSort(vector<int>& nums, int left, int right)
 {
 	if (left >= right) return;
+	srand(time(0));
+	int select_index = rand() % (right - left + 1) + left;
+	swap(nums[select_index], nums[left]);
 	int pivot = partition(nums, left, right);
 	quickSort(nums, left, pivot - 1);
 	quickSort(nums, pivot + 1, right);
